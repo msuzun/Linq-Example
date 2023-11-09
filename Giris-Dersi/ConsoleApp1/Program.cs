f71c9e71caeab19616f7808f355b9a259d98411f
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2023-11-09 23:49:47 +0300

    Giris-Dersi

---
+++ Giris-Dersi/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<string> countries = new List<string>();
+            countries.Add("India");
+            countries.Add("USA");
+            countries.Add("UK");
+            foreach (var country in countries)
+            {
+                Console.WriteLine(country);
+            }
+        }
+    }
+}
