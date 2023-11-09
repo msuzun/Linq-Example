6ed1be77f16d29621bc6543cb80f8dad89d8954a
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2023-11-10 00:21:29 +0300

    Linq-Syntax

---
+++ Linq-Syntax/ConsoleApp1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //QuerySyntax();
+            MethodSyntax();
+            Console.ReadLine();
+        }
+        static void QuerySyntax()
+        {
+            // 1 . yöntem Query Syntax
+            List<Student> students = new List<Student>();
+            students.Add(new Student
+            {
+                Id = 1,
+                Name = "Muhammet",
+            });
+            students.Add(new Student
+            {
+                Id = 2,
+                Name = "Şevki"
+            });
+            students.Add(new Student
+            {
+                Id = 3,
+                Name = "İbrahim"
+            });
+            var result = from s in students
+                         where s.Name.Contains("Ş")
+                         select s.Name;
+            Console.WriteLine(result.FirstOrDefault());
+        }
+        static void MethodSyntax()
+        {
+            // 1 . yöntem Query Syntax
+            List<Student> students = new List<Student>();
+            students.Add(new Student
+            {
+                Id = 1,
+                Name = "Muhammet",
+            });
+            students.Add(new Student
+            {
+                Id = 2,
+                Name = "Şevki"
+            });
+            students.Add(new Student
+            {
+                Id = 3,
+                Name = "İbrahim"
+            });
+            var result = students.Where(x => x.Name.Contains("Ş")).Select(c => c.Name);
+            Console.WriteLine(result.FirstOrDefault());
+        }
+    }
+   
+}
---
+++ Linq-Syntax/README.md
@@ -0,0 +1,2 @@
+# Linq-Syntax
+Linq da kullanabileceğimiz iki tür Syntax bulunmaktadır. Query Syntax ve Method Syntax. İki kullanımda örneklendirilmiştir
