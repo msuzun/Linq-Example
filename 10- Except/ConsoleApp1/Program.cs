5a719f85b7d77f78223eec9f0bddf68c90ef388a
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2023-11-12 02:01:05 +0300

    Except

---
+++ 10- Except/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Array1 = { "Ramesh", "Kapil", "Vikas", "Amit" };
+            string[] Array2 = { "Kapil", "Manmohan", "Deepak", "Amit" };
+            var fark = Array1.Except(Array2);
+            Console.WriteLine("Fark : ");
+            foreach (var item in fark)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 10- Except/README.md
@@ -0,0 +1,3 @@
+# Linq-Except-Kullanımı
+Elimizdeki bir dizinin parametre olarak verdiğimiz diziden farklarını alabilmemiz için kullanılır.
+Küme olarak düşünülürse A ve B kümeleri düşünelim. A kümesinin B kümesinden farklarını bulmamız gerekiyor ise A \B şeklinde alırız. Linq'da bunu Except ile yapıyoruz
