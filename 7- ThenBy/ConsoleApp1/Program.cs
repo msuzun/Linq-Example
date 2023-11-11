9e88a60f0ac500f6c17dfbdcb579745efac7a765
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2023-11-12 01:35:57 +0300

    ThenBy

---
+++ 7- ThenBy/ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Rank { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //QuerySyntax();
+            MethodSyntax();
+            Console.ReadLine();
+        }
+
+        private static void MethodSyntax()
+        {
+            List<Student> students = new List<Student>();
+            students.Add(new Student { Id = 1, Name = "Ramesh", Rank = 1, Age = 39 });
+            students.Add(new Student { Id = 2, Name = "Kapil", Rank = 1, Age = 32 });
+            students.Add(new Student { Id = 3, Name = "Suresh", Rank = 2, Age = 45 });
+            students.Add(new Student { Id = 4, Name = "Mahesh", Rank = 2, Age = 39 });
+
+            var studentsOrderByRank = students.OrderBy(x=>x.Rank).ThenBy(x=>x.Age);
+            Console.WriteLine("Sorted Students : ");
+            foreach (var student in studentsOrderByRank)
+            {
+                Console.WriteLine(student.Name);
+            }
+        }
+
+        private static void QuerySyntax()
+        {
+            List<Student> students = new List<Student>();
+            students.Add(new Student { Id = 1, Name = "Ramesh", Rank = 1, Age = 39 });
+            students.Add(new Student { Id = 2, Name = "Kapil", Rank = 1, Age = 32 });
+            students.Add(new Student { Id = 3, Name = "Suresh", Rank = 2, Age = 45 });
+            students.Add(new Student { Id = 4, Name = "Mahesh", Rank = 2, Age = 39 });
+
+            var studentsOrderByRank = from student in students
+                                      orderby student.Rank, student.Age
+                                      select student;
+            Console.WriteLine("Sorted Students : ");
+            foreach (var student in studentsOrderByRank)
+            {
+                Console.WriteLine(student.Name);
+            }
+        }
+    }
+}
---
+++ 7- ThenBy/README.md
@@ -0,0 +1,4 @@
+# Linq-ThenBy-Kullanımı
+OrdeyBy ile birden çok property'i sıralamak istediğimizde querysyntax ile aralarına virgül ekleyerek sıralı bir 
+şekilde bütün elemanları sıralayabiliriz. Yani ilk önce ilk parametremiz olmak üzere tüm parametreler sıralanır
+Method syntax ile bu kullanımı thenBy metotdu ile daha yalın bir hale getirdik.
