f3fca57883f4193e5f4cc8a0a90cb31b48afdccc
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2023-11-12 01:22:09 +0300

    OrderBy

---
+++ 6- OrderBy/ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Rank { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //QuerySyntax();
+            MethodSyntax();
+            Console.ReadLine();
+        }
+
+        private static void MethodSyntax()
+        {
+            List<Student> students = new List<Student>();
+            students.Add(new Student { Id = 1, Name = "Ramesh", Rank = 3 });
+            students.Add(new Student { Id = 2, Name = "Kapil", Rank = 1 });
+            students.Add(new Student { Id = 3, Name = "Suresh", Rank = 2 });
+
+            //var studentOrderByRank = students.OrderBy(x => x.Rank);
+            var studentOrderDescendingByRank = students.OrderByDescending(x => x.Rank);
+            Console.WriteLine("Sorted Students: ");
+            foreach (var student in studentOrderDescendingByRank)
+            {
+                Console.WriteLine(student.Name);
+            }
+        }
+
+        private static void QuerySyntax()
+        {
+            List<Student> students = new List<Student>();
+            students.Add(new Student { Id = 1, Name = "Ramesh", Rank = 3 });
+            students.Add(new Student { Id = 2, Name = "Kapil", Rank = 1 });
+            students.Add(new Student { Id = 3, Name = "Suresh", Rank = 2 });
+
+            var studentOrderByRank = from student in students
+                                     orderby student.Rank
+                                     select student;
+            Console.WriteLine("Sorted Students: ");
+            foreach (var student in studentOrderByRank)
+            {
+                Console.WriteLine(student.Name);
+            }
+        }
+    }
+}
---
+++ 6- OrderBy/README.md
@@ -0,0 +1,3 @@
+# Linq-OrderBy-Kullanımı
+Linq ile verileri sıralamak için sql'den aşina olduğumuz OrderBy kullanabiliriz. Dikkat etmemiz gereken konu ise
+QuerySyntax ile orderBy descending kullanamamamız. Burda methodsyntax ile kullandık
