bb9a5fefec1858b8222704c4e61eded7c152b64b
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2023-11-12 02:16:50 +0300

    Concat

---
+++ 12- Concat/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> sequence1 = new List<int> { 1,2,3,4};
+            List<int> sequence2 = new List<int> { 2,4,6,8};
+            var result = sequence1.Concat(sequence2);
+            foreach (var item in result)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("\nContat- Union farkı");
+            var result2 = sequence1.Union(sequence2);
+            foreach (var item in result2)
+            {
+                Console.Write(item + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 12- Concat/README.md
@@ -0,0 +1,3 @@
+# Linq-Concat-Kullanımı
+Kümeler mantığı ile kullanılan Union ile concat arasındaki fark concat aynı eleman olduğuna bakmaksızın verilerin
+liste veya diziyi birleştirir. Kodda karşılaştırma yapıldığı için net bir şekilde fark anlaşılabilir
