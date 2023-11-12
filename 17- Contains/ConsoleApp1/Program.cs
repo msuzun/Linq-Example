b63f899e2a094c9e57431c1b2039790944af2972
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2023-11-12 16:52:22 +0300

    Contains

---
+++ 17- Contains/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsFeesSubmitted { get; set; }
+        public int Age { get; set; }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> isimler = new List<string>();
+            isimler.Add("Sevki");
+            isimler.Add("Muhammet");
+            isimler.Add("İbrahim");
+            isimler.Add("Mustafa");
+
+            var result=isimler.Contains("Mustafa");
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 17- Contains/README.md
@@ -0,0 +1,2 @@
+# Linq-Contains-Kullanımı
+Dizi (Array) veya Kolleksiyon (Collection) içinde istenilen elemanın olup olmadığını bulabilmek için kullanabileceğimiz yöntemlerden biri "Contains" metodudur.
