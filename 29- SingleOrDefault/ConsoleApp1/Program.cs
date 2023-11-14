7c63fba62ab0783133f423e7ceccd4ba49323579
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2023-11-15 01:29:58 +0300

    SingleOrDefault

---
+++ 29- SingleOrDefault/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 10,20,30};
+            var x = numbers.Single(y=>y==20);
+            var z = numbers.SingleOrDefault(q=>q>40);
+            Console.WriteLine(x);
+            Console.WriteLine(z);
+            Console.ReadLine();
+        }
+    }
+}
