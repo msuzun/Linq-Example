0bce64df404ce15e1e56f3ce9bf41fe09d634855
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2023-11-15 01:33:03 +0300

    DefaultIfEmpty

---
+++ 30- DefaultIfEmpty/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 10,20,30};
+            var x = numbers.DefaultIfEmpty(8);
+            foreach (var item in x)
+            {
+                Console.WriteLine(item);
+            }
+           
+            Console.ReadLine();
+        }
+    }
+}
