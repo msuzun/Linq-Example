8ee80f2fd3fd8871c51fe9eb0212487a87ba8175
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2023-12-03 20:31:24 +0300

    Skip kullanımı

---
+++ 34- Skip/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 1,2,3,4,5,6,7,8,9,10};
+            //var result = numbers.Skip(4);
+            //var result = numbers.Where(x => x > 3).Skip(4);
+            //var result = numbers.Skip(4).Where(x => x < 8);
+            var result = (from x in numbers select x).Skip(4).Where(y=>y<8);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
