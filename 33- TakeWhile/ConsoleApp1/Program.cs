4ff2ab13d3d1830bbfc739dda496d3d88a231329
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2023-12-03 20:23:59 +0300

    TakeWhile

---
+++ 33- TakeWhile/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 1,2,3,6,7,8,9,10, 4, 5 };
+           var resultMS = numbers.TakeWhile(num=> num <6);
+
+            foreach (var item in resultMS)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
