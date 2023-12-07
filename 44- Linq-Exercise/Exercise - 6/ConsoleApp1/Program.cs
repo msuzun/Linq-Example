26a5dda4e93063b2b678b15924f855a46e6baf0a
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2023-12-08 02:50:10 +0300

    egzersiz 6

---
+++ 44- Linq-Exercise/Exercise - 6/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] haftagunleri = { "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi", "Pazar" };
+            var result = from gun in haftagunleri select gun;
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 44- Linq-Exercise/Exercise - 6/README.md
@@ -0,0 +1,2 @@
+# Linq-Exercise-6
+Haftanın gunlerini linq yardımıyla console yazdırınız
