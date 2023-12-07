9d5ae908bb730c4c06d7183d0e9c888339142f69
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2023-12-08 02:21:38 +0300

    Egzersiz 1

---
+++ 44- Linq-Exercise/Exercise - 1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var result1 = from sayi in array1 where sayi % 2 == 0 select sayi; // query syntax
+            var result2 = array1.Where(sayi=>sayi % 2 == 0); // method syntax
+            foreach (var item in result2)
+            {
+                Console.Write("{0} ",item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 44- Linq-Exercise/Exercise - 1/README.md
@@ -0,0 +1,2 @@
+# Linq-Exercise-1
+0'dan 10 kadar olan sayıları içeren bir dizi oluşturun. 2 ile bölümünden kalan 0 olanları konsola bastırın
