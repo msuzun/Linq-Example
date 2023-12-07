da52cb20a2672648be197288dc56f84a7612e1b8
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2023-12-08 02:46:42 +0300

    egzersiz 5

---
+++ 44- Linq-Exercise/Exercise - 5/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1;
+            Console.WriteLine("Bir kelime girin");
+            str1 = Console.ReadLine();
+            var result = from stryeni in str1 group stryeni by stryeni into sonuc select sonuc;
+
+            foreach (var item in result)
+            {
+                Console.WriteLine("Karater "+ item.Key + " "+ item.Count()+ " kere");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 44- Linq-Exercise/Exercise - 5/README.md
@@ -0,0 +1,2 @@
+# Linq-Exercise-5
+Bir karakter tanımlayın Bu karakteri oluşturan harfleri ve kaç kere kullanıldığını ekrana bastırın
