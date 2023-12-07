494b9265cefc282f41e2a9976b22cd4158353bfc
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2023-12-08 00:22:09 +0300

    SkipWhile

---
+++ 35- SkipWhile/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 1, 4, 5, 6, 7, 8, 9, 2, 3 };
+            var result = numbers.SkipWhile(x => x < 5);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 35- SkipWhile/README.md
@@ -0,0 +1,1 @@
+# Linq-SkipWhile-Kullanımı
