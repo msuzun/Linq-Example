6ae034f99e62d05e07d0b710a5667db67babf6ea
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2023-12-08 02:34:44 +0300

    Egzersiz 3

---
+++ 44- Linq-Exercise/Exercise - 3/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var array1 = new[] { 3, 5, 7, 9 };
+            var result = from sayi in array1
+                         let result2 = sayi * sayi
+                         where result2 > 10
+                         select new { sayi, result2 };
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 44- Linq-Exercise/Exercise - 3/README.md
@@ -0,0 +1,2 @@
+# Linq-Exercise-3
+Bir dizi tanımlayınız. Dizinin elemanlarını ve karesini console'a linq kullanarak basınız
