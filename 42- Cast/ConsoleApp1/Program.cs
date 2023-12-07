d867a747f741d700893e949454120ac938879e2b
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2023-12-08 02:09:36 +0300

    cast

---
+++ 42- Cast/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList array = new ArrayList()
+            {
+                10,
+                20,
+                30
+            };
+            array.Add("40");
+            IEnumerable<int> result = array.Cast<int>();
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 42- Cast/README.md
@@ -0,0 +1,1 @@
+# Linq-Cast-Kullanımı
