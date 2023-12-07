7959fa1cfff5c4cba71430ea0efdc3fd457156c4
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2023-12-08 00:52:56 +0300

    Range

---
+++ 37- Range/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //IEnumerable<int> number = Enumerable.Range(1, 10);
+            //IEnumerable<int> number = Enumerable.Range(1, 30).Where(sayi=>sayi %2 ==0);
+            IEnumerable<int> number = Enumerable.Range(1, 30).Select(sayi => sayi*sayi);
+            foreach (int i in number) { Console.WriteLine(i); };
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 37- Range/README.md
@@ -0,0 +1,1 @@
+# Linq-Range-Kullanımı
