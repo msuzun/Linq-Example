9fdae4e33f4a364a3a0c37f5439cd3a70db32ed2
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2023-12-08 02:28:37 +0300

    egzersiz 2

---
+++ 44- Linq-Exercise/Exercise - 2/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { -8, -9, 3, 9, 45, -4, 84, 14, 1, -98};
+            var maks = numbers.Max();
+            var result = from sayi in numbers where sayi > 0 where sayi <= maks select sayi;
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);   
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 44- Linq-Exercise/Exercise - 2/README.md
@@ -0,0 +1,2 @@
+# Linq-Exercise-2
+bir diziye rastgele sayilar atayin pozitif olanlarini where kullanarak consola yazdirin
