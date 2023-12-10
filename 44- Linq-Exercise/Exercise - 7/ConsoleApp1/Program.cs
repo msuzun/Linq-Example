9c2d630033065f4d57945d1a8f3a89b3ee368f3b
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2023-12-11 01:21:37 +0300

    egzersiz 7

---
+++ 44- Linq-Exercise/Exercise - 7/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dizim = new int[]{ 5,1,9,2,3,7,4,5,6,8,7,6,3,4,5,2};
+            var result = from x in dizim group x by x into y select y;
+            foreach (var item in result)
+            {
+                Console.WriteLine("Sayi: " + item.Key + " Sayi*Frequency: "+ item.Sum()+" Frequency: "+item.Count());
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 44- Linq-Exercise/Exercise - 7/README.md
@@ -0,0 +1,7 @@
+# Linq-Exercise-7
+Linq kullanarak aşağıdaki formatta çıktı üreten bir c# programı yazınız
+Test data
+the numbers in the array are:
+5,1,9,2,3,7,4,5,6,8,7,6,3,4,5,2
+expected output
+number number* frequency frequency
