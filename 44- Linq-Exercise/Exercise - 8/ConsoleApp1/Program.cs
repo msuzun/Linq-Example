d854aff79725771473604c9411efdb613dabf7ec
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2023-12-11 02:05:05 +0300

    egzersiz 8

---
+++ 44- Linq-Exercise/Exercise - 8/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS"};
+            string baslangic, bitis;
+            Console.WriteLine("Başlangıç karakteri giriniz");
+            baslangic = Console.ReadLine();
+            Console.WriteLine("Bitiş karakteri giriniz");
+            bitis = Console.ReadLine();
+
+            var result = from x in cities where x.ToLower().StartsWith(baslangic) where x.ToLower().EndsWith(bitis) select x;
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{baslangic} ile başlayan  ve {bitis} ile biten şehrimiz:  {item}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 44- Linq-Exercise/Exercise - 8/README.md
@@ -0,0 +1,9 @@
+# Linq-Exercise-8
+String tipinde bir dizi tanımlayınız. Dizi elemanlarından birini bulmak için console'a ilk harfini ve son harfini
+giriniz. Eşleşen dizi elemanını ekrana bastırınız.
+Test Data:
+The cities are: 'ROME','LONDON','NAIROBI','CALIFORNIA','ZURICH','NEW DELHI','AMSTERDAM','ABU DHABI','PARIS'
+Input starting character for the string : ABU
+Input ending character for the string : M
+Expected Output:
+The city starting with A and ending with M is : AMSTERDAM
