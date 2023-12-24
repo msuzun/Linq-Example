512160313d6da23af9202f35ac607d16460d5066
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2023-12-24 23:23:45 +0300

    Exercise 10

---
+++ 44- Linq-Exercise/Exercise - 10/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            short iteratorNumber;
+            List<int> tempList = new List<int>();
+            int tempS;
+            Console.WriteLine("Eleman Giriniz");
+            iteratorNumber = Convert.ToInt16(Console.ReadLine());
+
+            for (int i = 0; i < iteratorNumber; i++)
+            {
+                Console.WriteLine("Member {0}", i+1);
+                tempList.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            Console.WriteLine("Listelemek için hangi değerden başlasın");
+            tempS = Convert.ToInt32(Console.ReadLine());
+            var result = tempList.FindAll(x => x > tempS);
+
+            Console.WriteLine("-----------------------");
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 44- Linq-Exercise/Exercise - 10/README.md
@@ -0,0 +1,2 @@
+# Linq-Exercise-10
+Console'a integer bir liste için kaç adet eleman girileceğini yazın. O kadar elemanı giriniz. Daha sonra vereceğiniz değerden büyük olanları consola'a yazdırınz
