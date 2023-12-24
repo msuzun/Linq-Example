b7e514ae43ba68fe5b552a13b6642c25040a47c6
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2023-12-24 23:41:40 +0300

    Exercise 12

---
+++ 44- Linq-Exercise/Exercise - 12/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tempS;
+            Console.WriteLine("Bir cumle giriniz");
+            tempS = Console.ReadLine();
+
+            var tempS2 = FindUpperWord(tempS);
+            Console.WriteLine("Girmiş olduğunuz cümledeki büyük kelimeler");
+            foreach (var item in tempS2)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+        private static IEnumerable<string> FindUpperWord(string s) 
+        {
+           var result = s.Split(' ').Where(x=>string.Equals(x,x.ToUpper()));
+            return result;
+        }
+    }
+}
---
+++ 44- Linq-Exercise/Exercise - 12/README.md
@@ -0,0 +1,2 @@
+# Linq-Exercise-12
+Consola bir cümle yazınız. Bu cümlenin içerisinde geçen büyük harfli kelimeleri ekrana bastırınzı
