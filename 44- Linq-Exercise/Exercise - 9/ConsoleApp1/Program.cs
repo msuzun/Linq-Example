54e4075ca7de1fdab6b04fe234fcb3a8bd6e18a7
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2023-12-24 23:10:54 +0300

    Exercise 9

---
+++ 44- Linq-Exercise/Exercise - 9/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> tempList = new List<int>();
+            tempList.Add(55);
+            tempList.Add(200);
+            tempList.Add(155);
+            tempList.Add(51);
+            tempList.Add(95);
+            tempList.Add(498);
+            tempList.Add(53);
+
+            //var result = tempList.Where(x => x > 80);
+            var result = tempList.FindAll(x=>x>80 ? true : false);
+
+            foreach (var item in result) 
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 44- Linq-Exercise/Exercise - 9/README.md
@@ -0,0 +1,2 @@
+# Linq-Exercise-9
+İnteger tipinde bir liste tanımlayın bu liste de yer alan elemanlardan 80'den büyük olanları konsola yazdırınız
