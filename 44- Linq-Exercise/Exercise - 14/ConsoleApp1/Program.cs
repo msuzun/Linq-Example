ce49b3d4ccdef59b6633427b5d7e571b83c1fa3a
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2023-12-25 00:01:50 +0300

    Excerise 14

---
+++ 44- Linq-Exercise/Exercise - 14/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Puan { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> tempList = new List<Student>();
+            int n;
+            tempList.Add(new Student { Id = 1, Name = "Joseph",Puan = 800});
+            tempList.Add(new Student { Id = 2, Name = "Alex",Puan = 458});
+            tempList.Add(new Student { Id = 3, Name = "Harris",Puan = 900});
+            tempList.Add(new Student { Id = 4, Name = "Taylor",Puan = 900});
+            tempList.Add(new Student { Id = 5, Name = "Smith",Puan = 458});
+            tempList.Add(new Student { Id = 6, Name = "Natasa",Puan = 700});
+            tempList.Add(new Student { Id = 7, Name = "David",Puan = 750});
+            tempList.Add(new Student { Id = 8, Name = "Harry",Puan = 700});
+            tempList.Add(new Student { Id = 9, Name = "Nicolash",Puan = 597});
+            tempList.Add(new Student { Id = 10, Name = "Jenny",Puan = 750});
+
+            Console.WriteLine("Kaçıncı sıradaki öğrenciyi istiyorsunuz");
+
+            n= Convert.ToInt32(Console.ReadLine());
+            var result = (from s in tempList
+                         group s by s.Puan into deger
+                         orderby deger.Key descending
+                         select new
+                         {
+                             Record = deger.ToList()
+                         }).ToList();
+            result[n-1].Record.ForEach(x => Console.WriteLine("Id: {0},Name: {1},Puan: {2}",x.Id,x.Name,x.Puan));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 44- Linq-Exercise/Exercise - 14/README.md
@@ -0,0 +1,4 @@
+# Linq-Exercise-14
+Bir öğrenci listesi oluşturunuz. Bu listenin içerisinde öğrencilerin aldığı puanlar Id'ler ve isimler yer alsın.
+Kullanıcıya en yüksek kaçıncı nota sahip öğrenciyi görmek istediğini sorun. Cevaba göre öğrencinin ismini
+consola yazdırınız
