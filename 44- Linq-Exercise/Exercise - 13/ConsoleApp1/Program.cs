03bff58d0399692e4013319e176960500afa4872
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2023-12-24 23:46:47 +0300

    Exercise 13

---
+++ 44- Linq-Exercise/Exercise - 13/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array1;
+            int n;
+            Console.WriteLine("Dizi elemanının kaç elemanlı olacagini giriniz");
+            n = Convert.ToInt32(Console.ReadLine());
+            array1 = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Eleman {0}", i + 1);
+                array1[i] = Console.ReadLine();
+            }
+            string newString = string.Join(",", array1.Select(x=>x.ToString().ToArray()));
+            Console.WriteLine(newString);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 44- Linq-Exercise/Exercise - 13/README.md
@@ -0,0 +1,2 @@
+# Linq-Exercise-13
+Linq yardımıyla string bir diziyi tek bir string haline döüştürünüz
