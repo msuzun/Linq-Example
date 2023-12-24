9fced8dee660bad0515d60a2fe3f8529eda171e5
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2023-12-25 00:23:45 +0300

    Exercise 15

---
+++ 44- Linq-Exercise/Exercise - 15/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class ItemMust
+    {
+        public int ItemID { get; set; }
+        public string ItemDes { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ItemMust> tempList = new List<ItemMust>();
+            tempList.Add(new ItemMust { ItemID = 1 , ItemDes = "Biscuit"});
+            tempList.Add(new ItemMust { ItemID = 2 , ItemDes = "Honey"});
+            tempList.Add(new ItemMust { ItemID = 3 , ItemDes = "Butter"});
+            tempList.Add(new ItemMust { ItemID = 4 , ItemDes = "Brade"});
+            tempList.Add(new ItemMust { ItemID = 5 , ItemDes = "Honey"});
+            tempList.Add(new ItemMust { ItemID = 6 , ItemDes = "Biscuit"});
+
+            var reult = (from s in tempList select s.ItemDes).Distinct().OrderBy(x => x);
+            //var reult2 = (from s in tempList select s.ItemDes);
+            foreach (var item in reult)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 44- Linq-Exercise/Exercise - 15/README.md
@@ -0,0 +1,3 @@
+# Linq-Exercise-15
+Bir liste oluşturun ve listeyi doldurun. Listedeki elemanların her birini tek tek ayırıp sırada consola' yazdıran
+bir c# programı yazınız
