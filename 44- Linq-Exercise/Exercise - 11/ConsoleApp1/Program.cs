096c1908bafb990dc8c59148459c8855723cf89b
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2023-12-24 23:32:38 +0300

    Exercise 11

---
+++ 44- Linq-Exercise/Exercise - 11/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> tempList = new List<int>();
+            tempList.Add(15);
+            tempList.Add(75);
+            tempList.Add(55);
+            tempList.Add(85);
+            tempList.Add(25);
+            tempList.Add(35);
+            tempList.Sort();
+            tempList.Reverse();
+
+            foreach (var item in tempList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.Write("Deger: ");
+            short tempS = Convert.ToInt16(Console.ReadLine());
+            foreach (var item in tempList.Take(tempS))
+            {
+                   Console.WriteLine(item);
+            }
+            //for (int i = 0; i < tempS; i++)
+            //{
+            //    Console.WriteLine(tempList[i]);
+            //}
+            //foreach (var item in tempList.Take(3))
+            //{
+            //    Console.WriteLine(item);
+            //}
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 44- Linq-Exercise/Exercise - 11/README.md
@@ -0,0 +1,2 @@
+# Linq-Exercise-11
+İnteger tipinde bir dizi tanımlayınız. Dizinin elemanlarını giriniz. En yüksek 3 değeri ekrana yazdırınız.
